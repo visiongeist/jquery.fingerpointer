616fbed347794189b6e08edbdee9a414e451221d
Damien Antipa <dantipa@adobe.com>
2012-06-11 10:07:09 +0200

    initial files

--- README.md
+++ README.md
@@ -1,4 +1,29 @@
-jquery.fingerpointer
-====================
+# jquery.fingerpointer
 
-This plugin wraps the the jQuery event binding to provide different events for touch devices
+_fingerpointer_ is a tiny jQuery plugin to provide a simple switch between touch and mouse events. In the case you are planning to use the same web application for touch and desktop clients, you definitely need this plugin.
+
+- Lightweight (less than 300 bytes)
+- Stops messing up your event space
+- Simply based on jQuery 1.7 on/off methods
+- finger and pointer feels more natural
+
+## Example
+
+Usually you would bind a mouse event like that:
+> $('a').on('click', myFunc);
+
+So far there are no problems since click also works perfectly fine on touch events. Imagine you want to have different event for the same function call on touch events.
+> var isTouch = !!('ontouchstart' in window) ? 1 : 0; 
+>
+> if(isTouch) {
+>	$('a').on('taphold', myFunc);
+> } else {
+> 	$('a').on('click', myFunc);
+> }
+
+Now the way you can achieve that with fingerpointer:
+> $('a').fipo('taphold', 'click', myFunc);
+
+Pretty easy, right? But if you have some event call exclusively for touch or pointer devices you can use this fancy functions:
+> $('a').finger('taphold', 'click', myFunc);
+> $('a').pointer('click', 'click', myFunc);
---
+++ jquery.plugin.fingerpointer.js
@@ -0,0 +1,60 @@
+/*
+ * Fingerpointer jQuery plugin
+ * version 0.5
+ * author: Damien Antipa
+ * http://github.com/dantipa/jquery.fingerpointer
+ */
+(function( $ ){
+    var isTouch = !!('ontouchstart' in window) ? 1 : 0;
+
+    /**
+     *
+     * @param types
+     * @param selector
+     * @param data
+     * @param fn
+     * @param one
+     * @return {*}
+     */
+    $.fn.finger = function ( types, selector, data, fn, /*INTERNAL*/ one ) {
+        if (isTouch) {
+            this.on( types, selector, data, fn, one );
+        }
+        return this;
+    };
+
+    /**
+     *
+     * @param types
+     * @param selector
+     * @param data
+     * @param fn
+     * @param one
+     * @return {*}
+     */
+    $.fn.pointer = function ( types, selector, data, fn, /*INTERNAL*/ one ) {
+        if (!isTouch) {
+            this.on( types, selector, data, fn, one );
+        }
+        return this;
+    };
+
+    /**
+     *
+     * @param typesFinger
+     * @param typesPointer
+     * @param selector
+     * @param data
+     * @param fn
+     * @param one
+     * @return {*}
+     */
+    $.fn.fipo = function ( typesFinger, typesPointer, selector, data, fn, /*INTERNAL*/ one ) {
+        if (isTouch) {
+            this.on( typesFinger, selector, data, fn, one );
+        } else {
+            this.on( typesPointer, selector, data, fn, one );
+        }
+        return this;
+    };
+})( jQuery );
