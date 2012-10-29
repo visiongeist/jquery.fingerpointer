13d5ea5a96df1574333a8e88f13f2d549ec2c2d6
Damien Antipa <dantipa@adobe.com>
2012-10-29 08:57:07 +0100

    Rely on arguments
    Instead of using directly parameters rely on the arguments object.
    see Issue #3

--- jquery.plugin.fingerpointer.js
+++ jquery.plugin.fingerpointer.js
@@ -1,60 +1,34 @@
 /*
  * Fingerpointer jQuery plugin
- * version 1.0
+ * version 1.1
  * author: Damien Antipa
  * http://github.com/dantipa/jquery.fingerpointer
  */
-(function( $ ){
+(function ($, window, undefined) {
     var isTouch = 'ontouchstart' in window;
 
-    /**
-     *
-     * @param types
-     * @param selector
-     * @param data
-     * @param fn
-     * @param one
-     * @return {*}
-     */
-    $.fn.finger = function ( types, selector, data, fn, /*INTERNAL*/ one ) {
+    $.fn.finger = function () {
         if (isTouch) {
-            this.on( types, selector, data, fn, one );
+            this.on.apply(this, arguments);
         }
         return this;
     };
 
-    /**
-     *
-     * @param types
-     * @param selector
-     * @param data
-     * @param fn
-     * @param one
-     * @return {*}
-     */
-    $.fn.pointer = function ( types, selector, data, fn, /*INTERNAL*/ one ) {
+    $.fn.pointer = function () {
         if (!isTouch) {
-            this.on( types, selector, data, fn, one );
+            this.on.apply(this, arguments);
         }
         return this;
     };
 
-    /**
-     *
-     * @param typesFinger
-     * @param typesPointer
-     * @param selector
-     * @param data
-     * @param fn
-     * @param one
-     * @return {*}
-     */
-    $.fn.fipo = function ( typesFinger, typesPointer, selector, data, fn, /*INTERNAL*/ one ) {
-        if (isTouch) {
-            this.on( typesFinger, selector, data, fn, one );
-        } else {
-            this.on( typesPointer, selector, data, fn, one );
-        }
+    $.fn.fipo = function () {
+        var args = Array.prototype.slice.call(arguments, 1, arguments.length);
+
+        this.pointer.apply(this, args);
+
+        args[0] = arguments[0];
+        this.finger.apply(this, args);
+
         return this;
     };
-})( jQuery );
+}(jQuery, this));
